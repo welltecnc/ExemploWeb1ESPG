29a826b0972473edcf8c0a287ed201b3e72b7c9d
Wellington <welltecnic@hotmail.com>
2026-03-30 09:15:30 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
-alert("Hello World !!!")
+//Declarações e variáveis
+//var, let e const
+var nome="Fiap";
+console.log(nome);
+
+var nome="Teste";
+console.log(nome);
+
+if(true){
+    let apelido="Fiapinho"
+    console.log(apelido); 
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros);
+// taxaJuros=1.5;
+
+//typeof-verifica o tipo da variavel
+var exemplo="ola-dev-var";
+console.log(typeof exemplo)
+
+let exemplo1="ola-dev-let";
+console.log(exemplo1)
+
+const exemplo2="ola-dev-const";
+console.log(exemplo2)
+
+let exemplo3=10;
+console.log(typeof exemplo3)
+
+let exemplo4=true;
+console.log (typeof exemplo4);
+
+let exemplo5={nome1:"teste"};
+console.log( typeof exemplo5)
+
+let exemplo6=["Java","C#","PHP"];
+console.log(exemplo6)
+
+//valor undefined
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8 =null;
+console.log(exemplo8)
+
